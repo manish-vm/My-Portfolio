fe175296ac78eb6cabf32c1c05b09ca9ea566db8
Manish <127071839+manish-vm@users.noreply.github.com>
2025-12-26 11:02:07 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,12 +2,13 @@ const Navbar = () => {
     return(
         <>
         <nav className="navbar">
-            <h2 className="logo">Manish's Portfolio</h2>
-            <ul>
-                <li><a href="/about">About</a></li>
-                <li><a href="/skills">Skills</a></li>
-                <li><a href="/projects">Projects</a></li>
-                <li><a href="/contact">Contact</a></li>
+            {/* <h3 className="logo"><img src="/portfolio-logo.png" height="100px" width="100px" alt="logo" /><span>Manish's Portfolio</span></h3> */}
+            <img src="/portfolio-logo.png" height="150px" width="200px" alt="logo" className="logo1"/>
+            <ul className="nav-ul">
+                <li><a href="#about">About</a></li>
+                <li><a href="#skills">Skills</a></li>
+                <li><a href="#projects">Projects</a></li>
+                <li><a href="#contact">Contact</a></li>
             </ul>
         </nav>
         </>
